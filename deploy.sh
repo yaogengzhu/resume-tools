6b88fe795ebb955526637db60ba0e8d3cf282116
yaogengzhu <455947455@qq.com>
2022-04-09 15:21:55 +0800

    feat: 部署配置

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 # 发生错误时终止
 set -e
 
-# 构建
 npm run build
 
 # 进入构建文件夹
@@ -12,6 +11,8 @@ cd dist
 # 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
+git init
+git checkout -b master
 git add -A
 git commit -m 'deploy'
 
