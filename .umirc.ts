4003b8c99a278fc0770d3db3a3972ef915c42614
yaogengzhu <455947455@qq.com>
2022-04-09 15:16:33 +0800

    feat: 修改配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'umi'
+import { defineConfig } from 'umi';
 
 export default defineConfig({
   title: '文档生成器',
-  publicPath: ' https://yaogengzhu.github.io/resume-tools',
+  base: '/',
+  publicPath: 'https://yaogengzhu.github.io/resume-tools/',
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/',component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
-})
+});
